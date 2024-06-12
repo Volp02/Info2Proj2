f59c97d29ec77964813c3eebb76973d4a3f8d9da
bila1015 <lajosbilles@gmail.com>
2024-06-12 15:38:11 +0200

    winsoock

---
+++ initWinsock.cpp
@@ -0,0 +1,29 @@
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "initWinsock.h"
+
+#pragma comment(lib, "Ws2_32.lib")
+
+int initWinsock() {
+
+    WORD wVersionRequest;						//typedef für unsigned short = 16bit = 2byte
+    WSADATA wsaData;							//erschaffe Objekt des structs "WSDATA" (s. OneNote)
+    wVersionRequest = MAKEWORD(2, 2);			//V.2.2 der Winsock-API
+
+    int err = WSAStartup(wVersionRequest, &wsaData);	//gibt 0 zurück wenn alles gut läuft
+
+    if (err != 0)
+    {
+        cout << "WSAStartup failed with error: " << err << endl;
+        WSACleanup();
+        return err;
+    }
+    else
+    {
+        cout << "WSA Startup successful!" << endl;
+
+
+    }
+
+}
+    
