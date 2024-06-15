01157133726cab0f51900aba3cb3b171d6367a54
volp02 <bila1015@h-ka.de>
2024-06-15 20:07:16 +0200

    test

--- DataStorage.h
+++ DataStorage.h
@@ -4,4 +4,6 @@
 #include <iostream>
 
 std::vector<std::string> storeIP(std::vector<std::string>& IPStr, const std::string IP);
-std::string giveIP(std::vector<std::string>& IPStr);
+std::string giveIP(std::vector<std::string>& IPStr, int count);
+bool checkMessageID(std::vector<int>& MessageID_Vector, int MessageID);
+int createMessageID();
