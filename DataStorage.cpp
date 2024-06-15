5872bcfd812d1825d98e5c5460ba98b308c9b621
rungeAm <rungeandreasm@gmail.com>
2024-06-15 18:45:18 +0200

    id

--- DataStorage.cpp
+++ DataStorage.cpp
@@ -1,1 +1,28 @@
 #include "DatStorage.h"
+#ifdef _WIN32 // Windows-spezifischer Code
+#include "initWinsock.h"
+typedef int socklen_t;
+#else // Linux-spezifischer Code
+#include "linuxLib.h"
+#define closesocket close
+#endif
+
+#include <iostream>
+#include <string.h>
+#include <string>
+#include <thread>
+#include <vector>
+
+#include "connect.h"
+#include "listen.h"
+
+
+#define PORT 26000
+#define BUFFER_SIZE 1024
+
+
+int IPStorage()
+{
+
+	std::vector<std::string> knownIDs = { 0 };
+}
