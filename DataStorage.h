076752a1871f7e852412352e7a1978c959e8c52e
volp02 <bila1015@h-ka.de>
2024-06-15 20:19:39 +0200

    edit header files

--- DataStorage.h
+++ DataStorage.h
@@ -4,6 +4,6 @@
 #include <iostream>
 
 std::vector<std::string> storeIP(std::vector<std::string>& IPStr, const std::string IP);
-std::string giveIP(std::vector<std::string>& IPStr, int count);
+std::string giveIP(std::vector<std::string>& IPStr);
 bool checkMessageID(std::vector<int>& MessageID_Vector, int MessageID);
 int createMessageID();
